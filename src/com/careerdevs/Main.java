41fe08bc59583ab1a89bed948fb2882f7f707a97
Jeremy <jeremyeparker@yahoo.com>
2020-11-13 23:12:33 -0500

    start of conditionals

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -13,17 +13,28 @@ public class Main {
         System.out.print("What's the principal amount?: ");
         int principal = scanner.nextInt();
 
+        if(principal < 1000 || principal > 1000000) {
+            System.out.println("Please enter a principal between $1K and $1M ");
+        }
+
         final byte months_in_years = 12;
         final byte percent = 100;
 
         System.out.print("Annual Interest Rate?: ");
         float annualInterestRate = scanner.nextFloat();
 
+        if(annualInterestRate < 0 || annualInterestRate > 30 ){
+            System.out.println("Enter a value greater than 0 and less  than or equal 30");
+        }
+
         float monthInterestRate = annualInterestRate/ percent / months_in_years;
         System.out.println(monthInterestRate);
 
         System.out.print("Period (Years)? :");
         int years = scanner.nextInt(); //amount of monthly payments
+        if(years < 0 || years > 30){
+            System.out.println("Please an amount of years between 1 and 30");
+        }
         int numberOfPayments = years * months_in_years;
         System.out.println(numberOfPayments);
         double mortgage = principal * (monthInterestRate * Math.pow(1 + monthInterestRate,numberOfPayments)) / Math.pow(1 + monthInterestRate,numberOfPayments) - 1;
