5b37af1d54b984c154aab89c55b5e2884c22fa3c
Jeremy <jeremyeparker@yahoo.com>
2021-04-02 23:43:14 -0400

    calc

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,15 +1,26 @@
 package com.careerdevs;
 
 
-import java.util.Scanner;
-
 public class Main {
 
 
     public static void main(String[] args) {
 
-        MortgageCalc.MortgageCal();
+//        MortgageCalc.MortgageCal();
 
+    }
+}
+class Calculator{
+
+    int power(int n , int p) throws Exception{
+
+        if (n < 0 || p < 0){
+            throw new Exception("n and p should be non-negative");
+        }
+
+        int calc = (int) Math.pow(n,p);
+
+        return calc;
     }
 }
  
